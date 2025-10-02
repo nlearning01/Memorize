6cc6af391ac9baa6b45def0c960867f7f15351a4
srgxzhrv <sergey_sentybr@mail.ru>
2025-10-02 15:54:25 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Сергей Захаров on 02.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
